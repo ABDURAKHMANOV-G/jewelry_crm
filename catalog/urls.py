55b1e45f9c2e220b5cceb167f5a9c6402642be92
ABDURAKHMANOV-G <G.Abdurakhmanov@outlook.com>
2025-11-09 16:19:35 +0300

    Фикс бага с отображением шедевра

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('collection/', views.collection_view, name='collection'),
-    path('product/<int:product_id>/', views.product_detail_view, name='product_detail'),
+    path('collection/product/<int:product_id>/', views.product_detail_view, name='product_detail'),
+    path('collection/masterpiece/', views.masterpiece_view, name='masterpiece'),  # ← Новый URL
 ]
